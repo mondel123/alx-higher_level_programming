b51a62519be11405ccd329924a8fce0bb32e5274
mondel123 <adamsmonday1@gmail.com>
2022-12-02 14:42:39 -0800

    All changes committed

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """Print the value of the variable a from the variable_load_5."""
     from variable_load_5 import a
 
